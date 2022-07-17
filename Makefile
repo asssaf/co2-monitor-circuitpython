5ddeb0f2244dfc436fc1b97e3a4bf594e21e5308
assaf <asssaf@localhost>
2022-07-17 12:08:23 -0700

    add neopixel

--- Makefile
+++ Makefile
@@ -2,6 +2,9 @@ PYLINT = pylint
 PYLINTFLAGS = -rn
 
 PYTHONFILES := $(wildcard src/*.py)
+LIB_DIR := $(HOME)/dev/adafruit/adafruit-circuitpython-bundle-7.x-mpy-20220715/lib
+LIB_FILES := adafruit_il0373.mpy adafruit_scd4x.mpy neopixel.mpy
+LIB_PATHS := $(addprefix $(LIB_DIR)/,$(LIB_FILES))
 
 pylint: $(patsubst %.py,%.pylint,$(PYTHONFILES))
 
@@ -10,10 +13,17 @@ pylint: $(patsubst %.py,%.pylint,$(PYTHONFILES))
 
 
 fetch-deps:
-	@pip3 install -r requirements.txt -t lib
+	@pip3 install --upgrade -r requirements.txt -t lib
 
 clean-deps:
 	@rm -r lib
 
+deploy-libs:
+	install $(LIB_PATHS) /run/media/$(USER)/CIRCUITPY/lib/
+
 deploy: pylint
-	@cp src/code.py /run/media/assaf/CIRCUITPY/
+	install src/code.py /run/media/$(USER)/CIRCUITPY/
+
+all: deploy
+
+.PHONY: all fetch-deps clean-deps deploy pylint
--- src/code.py
+++ src/code.py
@@ -4,19 +4,22 @@ Air quality monitor
 import time
 
 import alarm # pylint: disable=import-error
-import board
 import adafruit_scd4x
+import board
+import neopixel
 
-DEEP_SLEEP_SECONDS = 20
+DEEP_SLEEP_SECONDS = 10
 WAIT_FOR_DATA_READY_SECONDS = 1
 WAIT_FOR_MEASUREMENTS_TRIES = 60
 
 print("Starting...")
+pixel = neopixel.NeoPixel(board.NEOPIXEL, 1)
+pixel.brightness = 0.1
+pixel.fill((0, 255, 0))
 
 i2c = board.I2C()
 scd4x = adafruit_scd4x.SCD4X(i2c)
 scd4x.self_calibration_enabled = False
-#print("Serial number:", [hex(i) for i in scd4x.serial_number])
 
 def get_measurement():
     """
@@ -31,6 +34,7 @@ def get_measurement():
             return
         time.sleep(WAIT_FOR_DATA_READY_SECONDS)
 
+
 def deep_sleep(seconds):
     """
     set up a wake up alarm and go to deep sleep
@@ -41,12 +45,31 @@ def deep_sleep(seconds):
     print(f"Going to sleep for {seconds} seconds...")
     alarm.exit_and_deep_sleep_until_alarms(time_alarm)
 
-scd4x.start_periodic_measurement()
-print("Waiting for first measurement....")
 
-get_measurement()
+def main():
+    """
+    Main entry point
+    """
+    #print("Serial number:", [hex(i) for i in scd4x.serial_number])
 
-scd4x.stop_periodic_measurement()
+    scd4x.start_periodic_measurement()
+    print("Waiting for first measurement....")
 
-deep_sleep(DEEP_SLEEP_SECONDS)
-# Does not return, so we never get here.
+    get_measurement()
+
+    scd4x.stop_periodic_measurement()
+
+    pixel.fill((0, 0, 0))
+
+    deep_sleep(DEEP_SLEEP_SECONDS)
+    # Does not return, so we never get here.
+
+
+try:
+    main()
+
+except Exception as e:
+    print(f"Error: {e}")
+    pixel.fill((255, 0, 0))
+    pixel.brightness = 0.1
+    raise
